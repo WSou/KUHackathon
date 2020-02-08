646429b068a5f7a131386ebe9dcca10de1cac40c
Wsou <watthana.southivong@hotmail.com>
2020-02-08 17:51:32 -0600

    Duck

---
+++ RnR.cpp
@@ -0,0 +1,21 @@
+if (){
+    condition = "You babysit as a side gig. you earned $1000!";
+    money += 1000;
+}
+if (){
+    double Earn = 7000;
+    switch(money)
+    {
+        case money <= 10000: 
+        money = money + (Earn * .9)
+        case money > 10000 && Money <= 20000: 
+        money = money + (Earn * .85)
+        case money > 20000 && Money <= 30000:
+        money = money + (Earn * .80)
+        case money > 30000 && Money <= 40000:
+        money = money + (Earn * .75)
+        case money > 40000
+        money = money + (Earn * .70)
+    }
+    condition = "Work at a minimum wage job. You have earned!";
+}
