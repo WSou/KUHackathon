793492b12112d259781138fa378acf268c891fcb
Wsou <watthana.southivong@hotmail.com>
2020-02-09 03:23:35 -0600

    update3

--- mapClass.cpp
+++ mapClass.cpp
@@ -6,14 +6,19 @@ void layout::mapGen()
 {
   srand(time(0));
 
-  for(int i = 0; i < 18; i++)
+  for(int i = 0; i < 8; i++)
   {
-    for(int j = 0; j < 18; j++)
+    for(int j = 0; j < 8; j++)
     {
-      mapArray[i][j] = rand() % 100 + 1;
+      mapArray[i][j] = rand() % 10 + 1;
+      std::cout <<mapArray[i][j] <<std::endl;
       tileState[i][j] = rand() % 8 + 1;
+      tileDesc[j];
+      std::cout <<tileDesc[j]<<std::endl;
+      std::cout <<tileState[i][j] <<std::endl;
     }
   }
+  mapArray[5][5] = 0;
 }
 
 int layout::mapInfo(int xCoord, int yCoord)
@@ -24,26 +29,28 @@ int layout::mapInfo(int xCoord, int yCoord)
 void layout::mapDraw()
 {
 
-  for(int i = 0; i < 19; i++)
+  for(int i = 1; i < 8; i++)
   {
-    for(int j = 0; j < 19; j++)
+    for(int j = 1; j < 8; j++)
     {
       int x = tileState[i][j];
       if (x < 5)
       {
         std::cout << "-";
       }
-      else if (x >= 5 && x <=6)
+      else if (x >= 5 && x <=7)
       {
         std::cout << "+";
       }
-      else if (x > 6)
+      else if (x > 8)
       {
         std::cout << "x";
       }
+      encName[x];
+      std::cout <<encName[x] <<std::endl;
 
 
-      if (j == 18)
+      if (j == 8)
       {
         std::cout << "\n";
       }
@@ -53,5 +60,5 @@ void layout::mapDraw()
 
 std::string layout::locationReturn(int xCoord, int yCoord)
 {
-  return ;
+  return " ";
 }
--- mapClass.hpp
+++ mapClass.hpp
@@ -7,8 +7,24 @@ private:
   int mapArray[9][9];
   int tileState[9][9];
   bool revealed[9][9];
-  std::string tileDesc[9] = {"Money Village","Mall Street","Bank of Money","Capital Two Mountain","Oak Industry","Getsla Company","Doogle University","GoGoDuck Farm","ShouldersBook Forest"};
-  std::string encounterName[9] = {"Elon Musky","Star Patrick","Obama Care","Yaylor Swooft","Devaughn James", "Cristiano Tomato","Charlie Putin","Lady Dada","Mark MyWord"};
+  std::string tileDesc[9] = {"Money Village","Mall Street","Bank of Money","Capital Two Mountain","Oak Industry","Getsla Company",
+                             "Doogle University","GoGoDuck Farm","ShouldersBook Forest"};
+  std::string encName[9] = {"Elon Musky","Star Patrick","Obama Care","Yaylor Swooft","Devaughn James", "Cristiano Tomato","Charlie Putin"
+                            ,"Lady Dada","Mark MyWord"};
+
+  std::string posEvent[5] = {"You babysit as a side gig. you earned $1000!","You did some labor work and got paid in cash","You bartended."
+                             "You earned $1000!","It's Tax time!","Work at a minimum wage job. You have earned 5000! But due to income taxes"};
+   double posEventNum[5] = {1000,2000,1500,5000};        
+
+  std::string neuEvent[3] = {"An old traveler walked up to you and asked for money in exchange for a\nlottery ticket. Would you like to give"
+                              "him 500 for a chance to win 20,000?\n","There's a great opportunity for you to start your own company! You will" 
+                              "losses 3 turns\nbut will gain a constant income of 1000\nEnter (y/n)","This is your chance to follow your dream!" 
+                              "You will losses 5 turns\nbut will gain a constant income of 3000.\n Become a Youtuber (y/n)?"};
+
+  std::string negEvent[5] = {"You are walking along the path and out of nowhere a thief pop up","You Is Dead. Like FR FR Dead DEAD","You were speeding "
+                              "and got pulled over by a cop. You were fined 1000","Your car broke down cause it hates you. You have to pay 3000",
+                              "While you were enjoying your vacation in hawaii. Your grandma appears from nowhere and beat you up. She stole 1000 from you"};
+  double negEventNum[5] =   {-4000,0,-1000,-3000,-1000};
 public:
   void mapGen();
   int mapInfo(int xCoord, int yCoord);
